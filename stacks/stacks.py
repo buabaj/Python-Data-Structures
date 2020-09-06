307a387fb0af0c4cc0a1a27c74bd0a1e98d63551
buabaj <buabajerry@gmail.com>
2020-09-06 17:01:12 +0000

    saving progress for the day

---
+++ stacks/stacks.py
@@ -0,0 +1,12 @@
+#code implementation of stacks in the python programming language
+
+#we create the whole stack as class and write the methods to perform functions on items in the stack
+class stack:
+
+    def __init__(self): #init method of the class
+
+        self.items = [] #instantiating an empty list so we can perform functions on
+
+    def add(self, item):
+        
+        self.item.append(item)
